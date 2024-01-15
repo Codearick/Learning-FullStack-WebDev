5e09d0b126f981794fd046c44d861a67643f8ff1
Codearick <rohan.gautam650@gmail.com>
2024-01-16 00:46:31 +0530

    update

--- Day-78(Ex-HackerTerminal)/script.js
+++ Day-78(Ex-HackerTerminal)/script.js
@@ -1,52 +1,57 @@
 document.body.style.backgroundColor = "green";
 let div = document.createElement("div");
+let div1 = document.createElement("div");
+let div2 = document.createElement("div");
+let div3 = document.createElement("div");
+let div4 = document.createElement("div");
 document.body.append(div);
 console.log("Script is running!");
 
-async function showDisplay(){
-    let rand = Math.floor(Math.random() * (7000 - 1000 + 1)) + 1000;
+async function showDisplay() {
+    let rand = Math.floor(Math.random() * (1000 - 1000 + 1)) + 1000;
 
-    let m1 = await new Promise ((resolve,reject)=> {
+    let m1 = await new Promise((resolve, reject) => {
         setTimeout(() => {
-           resolve(div.textContent = "Initializing Hacking");
-       }, rand);
+            resolve(div.textContent = "Initializing Hacking");
+        }, rand);
     });
 
-    let m2 = await new Promise ((resolve,reject)=>{
+    let m2 = await new Promise((resolve, reject) => {
         setTimeout(() => {
-            let div1 = createElement("div");
-            div1.textContent = "Reading your files...";
+            resolve(div1.textContent = "Reading your files...");
         }, rand);
     })
-    
-    let m3 = await new Promise ((resolve,reject)=>{
+
+    let m3 = await new Promise((resolve, reject) => {
         setTimeout(() => {
             div.append = "<p>Password Files Detected</p>";
         }, rand);
     })
-    
-    let m4 = await new Promise ((resolve,reject)=>{
+
+    let m4 = await new Promise((resolve, reject) => {
         setTimeout(() => {
             div.textContent = "Sending all the passwords and personal files to server";
         }, rand);
     })
-    
-    let m5 = await new Promise ((resolve,reject)=>{
+
+    let m5 = await new Promise((resolve, reject) => {
         setTimeout(() => {
             div.textContent = "Cleaning up";
         }, rand);
     })
 
-    console.log(rand);  
+    console.log(rand);
 }
-const main = ()=>{
-  showDisplay().then (a => {
-    console.log("Success!");
-  })
-}
+const main = () => {
+    Promiseall([showDisplay().m1,showDisplay.m2,showDisplay.m3,showDisplay.m4,showDisplay.m5]).then(a => {
+        console.log(a);
+    }).catch(error => {
+        console.log(error);
+    });
+} 
 main();
 
-function getDot(){
+function getDot() {
     setTimeout(() => {
         div.textContent = ".";
     }, 2000);
