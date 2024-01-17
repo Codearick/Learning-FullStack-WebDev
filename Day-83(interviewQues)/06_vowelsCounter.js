8d6d2859d86ffc03a7add9dd6cd985e55733f2c1
Codearick <rohan.gautam650@gmail.com>
2024-01-17 12:53:31 +0530

    jsQues added

--- Day-83(interviewQues)/06_vowelsCounter.js
+++ Day-83(interviewQues)/06_vowelsCounter.js
@@ -1,8 +1,15 @@
 // 6. The Vowel Counter:
 // You need to create a function that counts the number of vowels in a given string. Consider both uppercase and lowercase vowels.
-
 function vowelsCounter(str){
-    str = str.tolowercase();
+    let str1 = str.toLowerCase();
     let vowels = ['a','e','i','o','u'];
-    for(let i = 0)
+    let count = 0;
+    for(let i = 0; i<str1.length; i++){
+        if(vowels.indexOf(str1[i]) !== -1){
+            count++;
+        }
+    }
+    return count;
 }
+let str = "aaAeIoU";
+console.log("Number of vowels: ",vowelsCounter(str));
