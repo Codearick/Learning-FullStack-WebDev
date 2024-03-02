ca429be4bfea91bdeb70967711852db8d337e3f9
Codearick <rohan.gautam650@gmail.com>
2024-03-03 01:56:08 +0530

    update

---
+++ Day-122/server-components/app/page.js
@@ -0,0 +1,13 @@
+// import {useState, useEffect} from 'react'
+import Navbar from "@/components/Navbar";
+
+export default function Home() {
+  // const [count, setCount] = useState(0);
+  return (
+   <div>
+   <Navbar/>
+    I am a counting
+    {/* <button onClick={()=>setCount(count + 1)}>ADD</button> */}
+   </div>
+  );
+}
---
+++ Day-122/server-components/components/Navbar.js
@@ -0,0 +1,18 @@
+"useclient"
+import React from 'react'
+
+const Navbar = () => {
+    console.log("I am rohan");
+  return (
+    <div>
+        <ul>
+            <li>Home</li>
+            <li>Service</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
